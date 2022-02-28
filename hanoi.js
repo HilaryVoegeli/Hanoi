a4a4ce79ce3284a948e4724e23b9bbf6f3cc9a10
Hilary Voegeli <94722702+HilaryVoegeli@users.noreply.github.com>
2022-02-27 22:04:23 -0600

    Update hanoi.js

--- hanoi.js
+++ hanoi.js
@@ -1,9 +1,9 @@
 const writeAll = (pegMap) => {
   let s = JSON.stringify(pegMap);
-  // write output 
   console.log(s);
   pegHist.push(JSON.parse(s));
 };
+
 const pegHist = [];
 const pegMap = {
   A: [],
@@ -13,16 +13,13 @@ const pegMap = {
 const pegArray = ["A", "B", "C"];
 const updateMapAndPlot = (pegMap, from, to) => {
   try {
-    let theDisk = pegMap[from].pop(); // this actually moves the disk
+    let theDisk = pegMap[from].pop();
     pegMap[to].push(theDisk);
     writeAll(pegMap);
-  } catch (e) {
-    console.log("no disk in Map");
+  } catch (e) {console.log("no disk in Map");
   }
 };
 
-// Pegs  A   B   C
-// disks are labeled 0 to N
 var moves = 0;
 const moveDisks = function (n, from, to, spare) {
   if (n == 1) {
@@ -35,7 +32,7 @@ const moveDisks = function (n, from, to, spare) {
     moveDisks(n - 1, spare, to, from);
   }
 };
-// given from and to figure out the other peg
+
 const missing = (from, to) => {
   const all = {
     A: 1,
@@ -43,14 +40,14 @@ const missing = (from, to) => {
     C: 3
   };
   let total = all[from] + all[to];
-  let miss = 5 - total; // 0 based so not 6 but one less
-  let keys = Object.keys(all); // array of keys
+  let miss = 5 - total;
+  let keys = Object.keys(all);
   return keys[miss];
 };
 
 const initialize = (nDisks, pegMap) => {
   for (let i = nDisks; i >= 1; i--) {
-    pegMap["A"].push(i); // this stacks disks with smallest on top
+    pegMap["A"].push(i);
   }
   writeAll(pegMap);
 };
